243c71f786e84ea853e26b77ab9cbc5dd1b65ce2
augustin <cheron.augustin@gmail.com>
2026-05-29 14:44:47 +0200

    Fix car reader proto3 optional build

--- crates/old-faithful/car-reader/build.rs
+++ crates/old-faithful/car-reader/build.rs
@@ -1,21 +1,27 @@
-use std::io::Result;
+use std::error::Error;
 use std::path::PathBuf;
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=src/confirmed_block.proto");
 
-    prost_build::compile_protos(&["src/confirmed_block.proto"], &["src/"])?;
-
     let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").expect("OUT_DIR is set"));
-    let quick_out_dir = out_dir.join("quick-protobuf");
-    std::fs::create_dir_all(&quick_out_dir)?;
-
     let manifest_dir =
         PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR is set"));
     let proto = manifest_dir.join("src/confirmed_block.proto");
     let proto_dir = manifest_dir.join("src");
     let protos = [proto];
     let includes = [proto_dir];
+
+    let mut prost = prost_build::Config::new();
+    if std::env::var_os("PROTOC").is_none() {
+        prost.protoc_executable(protoc_bin_vendored::protoc_bin_path()?);
+    }
+    prost.protoc_arg("--experimental_allow_proto3_optional");
+    prost.compile_protos(&protos, &includes)?;
+
+    let quick_out_dir = out_dir.join("quick-protobuf");
+    std::fs::create_dir_all(&quick_out_dir)?;
+
     let config = pb_rs::ConfigBuilder::new(&protos, None, Some(&quick_out_dir), &includes)
         .expect("quick-protobuf config")
         .build();
